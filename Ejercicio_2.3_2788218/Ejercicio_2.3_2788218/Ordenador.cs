8bc84b1783bd4ec9ebb235b23e748de3958f47d4
Manuelito-Castillo <2788218@clases.edu.sv>
2025-02-17 22:00:13 -0600

    Add files via upload

---
+++ Ejercicio_2.3_2788218/Ejercicio_2.3_2788218/Ordenador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2._3_2788218
+{
+    public class Ordenador
+    {
+        
+
+        //Constructor
+        public Ordenador(string marca, string procesador, string memoria, string disco, double capacidad_del_disco)
+        {
+            this.marca = marca;
+            this.procesador = procesador;
+            this.memoria = memoria;
+            this.disco = disco;
+            this.capacidad_del_disco = capacidad_del_disco;
+        }
+        public string marca;            //Campos
+        public string procesador;
+        public string memoria;
+        public string disco;
+        public double capacidad_del_disco;
+        public double gb_incrementadas;
+
+        //Acciones
+        public void incrementar(double gb_incrementadas)
+        {
+            Console.WriteLine("José llevo a reparar su ordenador y las gb del disco aumentaron de " + capacidad_del_disco + " a " + gb_incrementadas + " gb.");
+        }
+        
+        public void reducir(double gb_reducidas)
+        {
+            Console.WriteLine("Sin embargo, el disco duro se daño y las gb de este se redujeron de " +capacidad_del_disco + " a " + gb_reducidas + " gb.");
+        }
+
+    }
+}
---
+++ Ejercicio_2.3_2788218/Ejercicio_2.3_2788218/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2._3_2788218
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ordenador ordenador = new Ordenador("hp", "Intel Core i3", "DDR4", "SSD", 100);
+            Console.WriteLine("José tiene un ordenador con las siguientes características: \n");
+            Console.WriteLine("- Marca: " + ordenador.marca);
+            Console.WriteLine("- Tipo de procesador: " + ordenador.procesador);
+            Console.WriteLine("- Tipo de memoria:  " + ordenador.memoria);
+            Console.WriteLine("- Tipo de disco: " + ordenador.disco);
+            ordenador.incrementar(200);
+            ordenador.reducir(0);
+
+            Console.ReadKey();
+        }
+    }
+}
